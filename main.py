697c8f7c3071cbec4fd311e77c6680c81499e187
CichyJohnny <04cichy@gmail.com>
2023-11-08 20:00:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import datetime, time
+app = FastAPI()
+
+x = datetime.date(2024, 1, 13)
+y = datetime.date.today()
+z = datetime.date.today().year
+odlic = str(x-y)
+
+@app.get("/")
+async def root():
+    return f'zostałoooo: {odlic}'
+
+
+@app.get("/items/{id}")
+async def read(id):
+    return {
+        'data': {
+            'name': id,
+            'age': z - 20030000,
+            'odliczanie': ' '.join((str(x-y)).split()[:2])
+        }
+    }
